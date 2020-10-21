4b53b6b5e1bc012f5dc5ccb23cdfe185f48058a2
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-21 23:07:36 +0300

    2-Tours Fetch Data

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -4,7 +4,37 @@ import Tours from './Tours';
 const url = 'https://course-api.netlify.app/api/react-tours-project';
 
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    }
+    
+  }
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return <main>
+      <Loading />
+    </main>
+  }
+
+  return <main>
+    <Tours />
+  </main>;
 }
 
 export default App;
