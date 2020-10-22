13fe7e5ccbb128d199bd4d444cb1b8cd6f68b800
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-22 03:24:58 +0300

    6-Tabs setup

--- 6-tabs/setup/src/App.js
+++ 6-tabs/setup/src/App.js
@@ -1,8 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import { FaAngleDoubleRight } from 'react-icons/fa';
 const url = 'https://course-api.netlify.app/api/react-tabs-project';
+
 function App() {
-  return <h2>tabs project setup</h2>;
+  const [loading, setLoading] = useState(true);
+  const [value, setValue] = useState(0);
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async () => {
+    try {
+      const response = await fetch(url);
+      const newJobs = await response.json();
+      setJobs(newJobs);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return <section className="section loading">
+      <h2>Loading...</h2>
+    </section>
+  }
+
+  return <main>
+
+  </main>;
 }
 
 export default App;
