049fbae1d9f94ee01dae6d3dc527ef8f64f9bc44
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-22 22:05:24 +0300

    9-Color-Generator Copy to clipboard

--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -4,12 +4,24 @@ const SingleColor = ({index, rgb, weight, hexColor}) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(',');
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 4000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
   return (
     <article className={`color ${index > 10 && 'color-light'}`}
      style={{backgroundColor:`rgb(${bcg})`}}
+     onClick={() => {
+       setAlert(true);
+       navigator.clipboard.writeText(`#${hexColor}`);
+    }}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">#{hexColor}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
     </article>
   );
 }
