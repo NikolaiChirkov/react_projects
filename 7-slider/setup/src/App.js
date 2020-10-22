3fc6600d1a1325bb00c2ad64ee249486a1d2d52c
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-22 20:23:27 +0300

    7-Slider setup classes

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -21,7 +21,17 @@ function App() {
       {people.map((person, personIndex) => {
         const {id, image, name, title, quote} = person;
 
-        return <article key={id}>
+        let position = 'nextSlide';
+
+        if (personIndex === index) {
+          position = 'activeSlide';
+        }
+
+        if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
+          position = 'prevSlide'
+        }
+
+        return <article className={position} key={id}>
           <img src={image} alt={name} className="person-img"/>
           <h4>{name}</h4>
           <p className="title">{title}</p>
