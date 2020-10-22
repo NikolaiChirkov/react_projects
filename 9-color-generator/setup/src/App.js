4e3bcc302b8540fdd66f0a5364f1a8938c6b5ce1
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-22 21:55:48 +0300

    9-Color-Generator Single Color

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -6,13 +6,14 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('');
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values('#f15025').all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
     
     try {
-      let colors = new Values().all(10);
+      let colors = new Values(color).all(10);
+      setList(colors);
     } catch(error) {
       setError(true);
       console.error(error);
@@ -26,15 +27,17 @@ function App() {
         <input 
           type="text" 
           value={color} 
-          placeholder='#f15025'
           onChange={(e) => setColor(e.target.value)}
+          placeholder='#f15025'
           className={`${error ? 'error' : null}`}
         />
         <button className="btn" type="submit">submit</button>
       </form>
     </section>
     <section className="colors">
-
+      {list.map((color, index) => {
+        return <SingleColor key={index} {...color} index={index} hexColor={color.hex}/>
+      })}
     </section>
   </>
 }
--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({index, rgb, weight, hexColor}) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(',');
+
+  return (
+    <article className={`color ${index > 10 && 'color-light'}`}
+     style={{backgroundColor:`rgb(${bcg})`}}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">#{hexColor}</p>
+    </article>
+  );
 }
 
 export default SingleColor
