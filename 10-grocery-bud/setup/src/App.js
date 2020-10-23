9b3e31625ab56825bdf54658f3c1b4a23265860b
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-23 05:44:09 +0300

    10-Grocery-Bud add items

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -11,6 +11,16 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!name) {
+
+    } else if (name && isEditing) {
+
+    } else {
+
+      const newItem = {id: new Date().getTime.toString(), title: name};
+      setList([...list, newItem]);
+      setName('');
+    }
   }
 
   return <section className="section-center">
@@ -30,10 +40,12 @@ function App() {
         </button> 
       </div>
     </form>
-    <div className="grocery-container">
-      <List />
-      <button className="clear-btn">clear items</button>
-    </div>
+    {list.length > 0 && 
+      <div className="grocery-container">
+        <List items={list}/>
+        <button className="clear-btn">clear items</button>
+      </div>
+    }
   </section>
 }
 
