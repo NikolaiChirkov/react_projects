56d56d4d36a79ac15b56169be60d934617567801
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-23 09:04:36 +0300

    11-Navbar simple toggle

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,32 +1,35 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
+import { FaBars } from 'react-icons/fa'
 import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  const [] = useState();
+  const [showLinks, setShowLinks] = useState(false);
 
   useEffect(() => {
 
   }, []);
+
   return <nav>
     <div className="nav-center">
       <div className="nav-header">
         <img src={logo} alt="logo" />
-        <button className="nav-toggle">
+        <button className="nav-toggle" onClick={() => setShowLinks(!showLinks)}>
           <FaBars />
         </button>
       </div>
-      <div className="links-container show-container">
-        <ul className="links">
-          {links.map(link => {
-            const {id, url, text} = link;
-            return <li key={id}>
-              <a href={url}>{text}</a>
-            </li>
-          })}
-        </ul>
-      </div>
+      {showLinks && 
+        <div className="links-container show-container">
+          <ul className="links">
+            {links.map(link => {
+              const {id, url, text} = link;
+              return <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            })}
+          </ul>
+        </div>
+      }
       <ul className="social-icons">
         {social.map((link) => {
           const {id, url, icon} = link;
