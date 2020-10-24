e540ca7cc6b259a3dbdb9d6344b47ba33337c25f
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-24 20:38:37 +0300

    14-Cart remove item

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -1,7 +1,15 @@
+import CartItem from "./CartItem";
+
 const reducer = (state, action) => {
     switch (action.type) {
         case 'CLEAR_CART':
             return {...state, cart: []}
+        break;
+        case 'REMOVE_ITEM':
+            return {
+                ...state, 
+                cart: state.cart.filter((CartItem) => CartItem.id !== action.payload)
+            }
         break;
     }
 
