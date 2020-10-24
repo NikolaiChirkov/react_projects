e75e5c319f76e6fb1a7842de652b891296928027
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-24 20:31:57 +0300

    14-Cart clear cart

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -1,4 +1,10 @@
 const reducer = (state, action) => {
+    switch (action.type) {
+        case 'CLEAR_CART':
+            return {...state, cart: []}
+        break;
+    }
+
     return state;
 }
 
