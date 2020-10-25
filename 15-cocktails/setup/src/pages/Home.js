0200531ee310a2aa8394635c33783c16a5ec9640
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-25 21:45:13 +0300

    15-cocktails context setup

--- 15-cocktails/setup/src/pages/Home.js
+++ 15-cocktails/setup/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import Cocktail from '../components/Cocktail'
 import CocktailList from '../components/CocktailList'
 import SearchForm from '../components/SearchForm'
 
 const Home = () => {
   return (
-    <div>
-      <h2>home page</h2>
-    </div>
+    <main>
+      <SearchForm />
+      <CocktailList />
+    </main>
   )
 }
 
